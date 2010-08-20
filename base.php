30fa0f29dd1e7d8bd9ab77973cb9be94f5ef10f1
Boone Gorges <boonebgorges@gmail.com>
2010-08-20 17:36:19 -0400

    Super skeletony base.php

--- base.php
+++ base.php
@@ -1,6 +1,26 @@
 <?php
 
-// Loader will go here
+// Load the tei-dom class
+require_once( ANTHOLOGIZE_TEIDOM_PATH );
+
+// Get the TEI Dom
+$teidom = new TeiDom( $_SESSION );
+
+// Do some fancy stuff to $teidom. These are the magical moments.
+
+// Filename and extension
+$fileName = TeiDom::getFileName( $_SESSION );
+$ext = "xml";
+
+// Send the proper headers
+header("Content-type: application/xml");
+header("Content-Disposition: attachment; filename=$fileName.$ext");
+
+// Echo your content
+echo $teidom->getTeiString();
+
+
+
 
 
 ?>
